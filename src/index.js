83400e44df6d8918ed9c58c5112d5b50978d5282
Andrew Clark <aiandrew631@gmail.com>
2024-11-18 22:48:51 -0600

    add more components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,100 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const pizzaData = [
+    {
+        name: 'Focaccia',
+        ingredients: 'Bread with italian olive oil and rosemary',
+        price: 6,
+        photoName: 'pizzas/focaccia.jpg',
+        soldOut: false,
+    },
+    {
+        name: 'Pizza Margherita',
+        ingredients: 'Tomato and mozarella',
+        price: 10,
+        photoName: 'pizzas/margherita.jpg',
+        soldOut: false,
+    },
+    {
+        name: 'Pizza Spinaci',
+        ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+        price: 12,
+        photoName: 'pizzas/spinaci.jpg',
+        soldOut: false,
+    },
+    {
+        name: 'Pizza Funghi',
+        ingredients: 'Tomato, mozarella, mushrooms, and onion',
+        price: 12,
+        photoName: 'pizzas/funghi.jpg',
+        soldOut: false,
+    },
+    {
+        name: 'Pizza Salamino',
+        ingredients: 'Tomato, mozarella, and pepperoni',
+        price: 15,
+        photoName: 'pizzas/salamino.jpg',
+        soldOut: true,
+    },
+    {
+        name: 'Pizza Prosciutto',
+        ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+        price: 18,
+        photoName: 'pizzas/prosciutto.jpg',
+        soldOut: false,
+    },
+]
 
 function App() {
-  return <h1>Hello, World!</h1>;
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+function Header() {
+  return(
+    <header>
+      <h1>Fast React Pizza Company</h1>
+    </header>
+  )
+}
+
+function Menu() {
+  return(
+    <div>
+      <h2>Menu</h2>
+        <Pizza />
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+      <footer>
+          <p>{new Date().toLocaleTimeString()}We're currently open</p>
+      </footer>
+  )
+}
+
+function Pizza() {
+    return (
+        <div>
+            <img src='pizzas/focaccia.jpg' alt='Focaccia' />
+            <h2>Pizza</h2>
+            <p>sfafds</p>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
