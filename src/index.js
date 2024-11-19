05d0488ea36bd518857f02db5b7e5710113ef06a
Andrew Clark <aiandrew631@gmail.com>
2024-11-19 02:54:48 -0600

    Moved pizzaData to separate module, added footer text alignment in CSS, and refactored index.js to import pizzaData.

--- src/index.js
+++ src/index.js
@@ -1,51 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import pizzaData from './data'
 
-const pizzaData = [
-    {
-        name: 'Focaccia',
-        ingredients: 'Bread with italian olive oil and rosemary',
-        price: 6,
-        photoName: 'pizzas/focaccia.jpg',
-        soldOut: false,
-    },
-    {
-        name: 'Pizza Margherita',
-        ingredients: 'Tomato and mozarella',
-        price: 10,
-        photoName: 'pizzas/margherita.jpg',
-        soldOut: false,
-    },
-    {
-        name: 'Pizza Spinaci',
-        ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
-        price: 12,
-        photoName: 'pizzas/spinaci.jpg',
-        soldOut: false,
-    },
-    {
-        name: 'Pizza Funghi',
-        ingredients: 'Tomato, mozarella, mushrooms, and onion',
-        price: 12,
-        photoName: 'pizzas/funghi.jpg',
-        soldOut: false,
-    },
-    {
-        name: 'Pizza Salamino',
-        ingredients: 'Tomato, mozarella, and pepperoni',
-        price: 15,
-        photoName: 'pizzas/salamino.jpg',
-        soldOut: true,
-    },
-    {
-        name: 'Pizza Prosciutto',
-        ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
-        price: 18,
-        photoName: 'pizzas/prosciutto.jpg',
-        soldOut: false,
-    },
-]
+  
 
 function App() {
     return (
@@ -54,16 +12,16 @@ function App() {
             <Menu />
             <Footer />
         </div>
-    );
-};
+    )
+}
 
 function Header() {
     return (
         <header className='header'>
             <h1>Fast React Pizza Company</h1>
         </header>
-    );
-};
+    )
+}
 function Menu() {
     return (
         <main className='menu'>
@@ -78,7 +36,7 @@ function Menu() {
                 />
             ))}
         </main>
-    );
+    )
 }
 
 function Pizza({ name, ingredients, photoName, price }) {
@@ -94,27 +52,28 @@ function Pizza({ name, ingredients, photoName, price }) {
 }
 
 function Footer() {
-    const hour = new Date().getHours();
-    const openHour = 12;
-    const closeHour = 22;
-    const isRestaurantOpen = hour >= openHour && hour <= closeHour;
+    const hour = new Date().getHours()
+    const openHour = 12
+    const closeHour = 22
+    const isRestaurantOpen = hour >= openHour && hour <= closeHour
 
-    isRestaurantOpen ? <p>'We are open'</p> : <p>'We are closed'</p>
-    if (isRestaurantOpen) {
-        console.log('We are open');
-    } else {
-        console.log('We are closed');
-    }
     return (
         <footer className='footer'>
-            <p>{new Date().toLocaleTimeString()}</p>
+            <p>
+                {new Date().toLocaleTimeString()}{' '}
+                {isRestaurantOpen ? (
+                    <p>'We are open'</p>
+                ) : (
+                    <p>'We are closed'</p>
+                )}
+            </p>
         </footer>
-    );
+    )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
-);
+)
